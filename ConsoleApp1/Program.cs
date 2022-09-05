76f9ea42ac440681cf9cff99243244e876f043b8
SNJV229 <38901682+SNJV229@users.noreply.github.com>
2022-09-06 00:37:20 +0530

    First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Singleton
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = Counter.Instance;
+            counter.incrementCounter();
+            counter.decrementCounter();
+        }
+    }
+
+    public class Counter
+    {
+        public int count = 0;
+        public static Counter instance = null;
+        private Counter() {  }
+        public static Counter Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Counter();
+                }
+                return instance;
+            }
+        }
+
+        public void incrementCounter()
+        {
+            
+            count += 1;
+            Console.WriteLine(count);
+            //Console.WriteLine("Hi");
+            Console.ReadLine();
+        }
+
+        public void decrementCounter()
+        {
+
+            count -= 1;
+            Console.WriteLine(count);
+            //Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+
+
+    }
+}
